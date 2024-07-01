ebb377b797c54e2df1c9f3b960fe76d8e8b1c0e9
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-07-01 20:35:52 +0600

    developed: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/hero";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
